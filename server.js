cf13ca23905148c8a80b63630ea01c5262d9a312
Md Shohag Showdagor <mdshohagshowdagor001@gmail.com>
2026-09-08 17:34:09 +0600

    added first query parameter

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const { error } = require('console');
 let highestPriceData;
 let lowestPriceData;
 let mostPopularData;
@@ -16,8 +17,29 @@ catch (err) {
 }
 app.use(express.static(__dirname));
 app.get("/get-property", (req, res) => {
-    // res.send("Property returned again");
-    res.json(highestPriceData);
+    let numberofQuery = 0;
+    let mostPopular = Boolean(req.query['most-popular']);
+    if (mostPopular) numberofQuery++;
+    let lowestPrice = Boolean(req.query['lowest-price']);
+    if (lowestPrice) numberofQuery++;
+    let highestPrice = Boolean(req.query['highest-price']);
+    if (highestPrice) numberofQuery++;
+    if (numberofQuery > 1) {
+        res.status(422).json({ error: "You cannot use more than one parameter" });
+    }
+    let limit = Number(req.query.limit);
+    let resposeData;
+    if (mostPopular) {
+        responseData = {
+            ...mostPopularData,
+            Result: {
+                ...mostPopularData.Result,
+                Items: mostPopularData.Result.Items.slice(0, limit)
+            }
+        };
+        res.json(responseData);
+    }
+    res.json(resposeData);
 });
 app.listen(3000,
     () => console.log("Server running on port 3000 \n You can visit http://localhost:3000 to access it"));
