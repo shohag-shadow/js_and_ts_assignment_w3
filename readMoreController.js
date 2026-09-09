68760ef42a1cd19479a48af99c984d7e4a01a17e
Md Shohag Showdagor <mdshohagshowdagor001@gmail.com>
2026-09-09 17:06:58 +0600

    solved overlay issue in read more

--- readMoreController.js
+++ readMoreController.js
@@ -1,15 +1,20 @@
 let readMoreButton = document.getElementById("read-more-button");
 let buttonStatus = false;
 let info = document.getElementById("site-inforation");
+
 function readMoreButtonToggle() {
     buttonStatus = !buttonStatus;
     if (buttonStatus) {
         info.classList.add("site-course-info__description--read-less");
-        readMoreButton.innerText = "READ LESS";
-    }
-    else {
+        readMoreButton.innerHTML = `READ LESS <span class="arrow">▲</span>`;
+        info.style.paddingBottom = "20px";
+    } else {
         info.classList.remove("site-course-info__description--read-less");
-        readMoreButton.innerText = "READ MORE";
+        readMoreButton.innerHTML = `READ MORE <span class="arrow">▼</span>`;
     }
 }
+
 readMoreButton.addEventListener("click", readMoreButtonToggle);
+
+// Initial arrow (set on page load)
+readMoreButton.innerHTML = `READ MORE <span class="arrow">▼</span>`;
